fbe0d1dc544611140d728e9d50d4b7f458a43698
John Shiver <johneshiver@gmail.com>
2016-02-16 23:09:43 -0800

    added foundation for delete method

--- tests/test_trees.py
+++ tests/test_trees.py
@@ -19,67 +19,77 @@ class TestBinaryTree(unittest.TestCase):
 
     def test_add_right(self):
         self.root.value = 5
-        self.root.add_tree(10)
+        self.root.add(10)
         self.assertEqual(10, self.root.right.value)
 
     def test_add_left(self):
         self.root.value = 5
-        self.root.add_tree(1)
+        self.root.add(1)
         self.assertEqual(1, self.root.left.value)
 
     def test_add_right_parent(self):
         self.root.value = 5
-        self.root.add_tree(10)
+        self.root.add(10)
         self.assertEqual(self.root, self.root.right.parent)
 
     def test_add_left_parent(self):
         self.root.value = 5
-        self.root.add_tree(1)
+        self.root.add(1)
         self.assertEqual(self.root, self.root.left.parent)
 
     def test_add_two_right(self):
         self.root.value = 5
-        self.root.add_tree(10)
-        self.root.add_tree(15)
+        self.root.add(10)
+        self.root.add(15)
         self.assertEqual(10, self.root.right.value)
         self.assertEqual(15, self.root.right.right.value)
 
     def test_add_two_left(self):
         self.root.value = 5
-        self.root.add_tree(3)
-        self.root.add_tree(1)
+        self.root.add(3)
+        self.root.add(1)
         self.assertEqual(3, self.root.left.value)
         self.assertEqual(1, self.root.left.left.value)
 
     def test_add_two_right_one_nested_left(self):
         self.root.value = 5
-        self.root.add_tree(10)
-        self.root.add_tree(15)
-        self.root.add_tree(13)
+        self.root.add(10)
+        self.root.add(15)
+        self.root.add(13)
         self.assertEqual(13, self.root.right.right.left.value)
 
     def test_add_two_left_one_nested_right(self):
         self.root.value = 15
-        self.root.add_tree(10)
-        self.root.add_tree(3)
-        self.root.add_tree(7)
+        self.root.add(10)
+        self.root.add(3)
+        self.root.add(7)
         self.assertEqual(7, self.root.left.left.right.value)
 
     def test_search_for_simple_right(self):
         self.root.value = 10
-        self.root.add_tree(15)
+        self.root.add(15)
         self.assertEqual(15, self.root.search_for_value(15).value)
 
     def test_search_for_simple_left(self):
             self.root.value = 10
-            self.root.add_tree(5)
+            self.root.add(5)
             self.assertEqual(5, self.root.search_for_value(5).value)
 
     def test_search_two_left_one_nested_right(self):
         self.root.value = 15
-        self.root.add_tree(10)
-        self.root.add_tree(3)
-        self.root.add_tree(7)
+        self.root.add(10)
+        self.root.add(3)
+        self.root.add(7)
         self.assertEqual(3, self.root.search_for_value(3).value)
 
-    # def test_search_for
+    def test_delete_left(self):
+        self.root.value = 10
+        self.root.add(5)
+        self.root.delete(5)
+        self.assertIsNone(self.root.left)
+
+    def test_delete_right(self):
+        self.root.value = 10
+        self.root.add(15)
+        self.root.delete(15)
+        self.assertIsNone(self.root.right)
--- trees/binary_tree.py
+++ trees/binary_tree.py
@@ -25,19 +25,46 @@ class BinaryTree(object):
         else:
             self.compare = compare
 
-    def add_tree(self, new_value):
+    def add(self, new_value):
         if self.compare(new_value, self.value):
             if not self.right:
                 self.right = BinaryTree(new_value)
                 self.right.parent = self
             else:
-                self.right.add_tree(new_value)
+                self.right.add(new_value)
         else:
             if not self.left:
                 self.left = BinaryTree(new_value)
                 self.left.parent = self
             else:
-                self.left.add_tree(new_value)
+                self.left.add(new_value)
+
+    def delete(self, delete_value):
+        # TODO: this is under construction
+        if delete_value is None:
+            raise ValueError("Cannot delete None!")
+        try:
+            deleted_node = self.search_for_value(delete_value)
+        except ValueError:
+            "Couldnt delete node, it wasnt there!"
+        else:
+            if not deleted_node.parent:
+                raise ValueError("cant delete the root node!")
+            if deleted_node.parent.right == deleted_node:
+                if not deleted_node.right and not deleted_node.left:
+                    deleted_node.parent == None
+                    deleted_node.parent.right == None
+
+                else:
+                    deleted_node.parent.right = max(filter(None, deleted_node.left, deleted_node.right))
+            else:
+                if not deleted_node.right and not deleted_node.left:
+                    deleted_node.parent == None
+                    deleted_node.parent.left == None
+                else:
+                    deleted_node.parent.right = max(filter(None, deleted_node.left, deleted_node.right))
+
+        return deleted_node
 
     def search_for_value(self, search_value):
         if search_value is None:
@@ -49,3 +76,4 @@ class BinaryTree(object):
         if self.left:
             return self.left.search_for_value(search_value)
         raise ValueError("{} not in my tree", search_value)
+
