604feedaa0005cffcc854315d1bb5830fe2c692b
John Shiver <johneshiver@gmail.com>
2016-07-12 23:09:27 -0400

    started work on graphs

---
+++ data_structures/graphs/graph.py
@@ -0,0 +1,24 @@
+
+
+class Graph(object):
+
+    def __init__(self):
+        self.num_vertices = 0
+        self.num_edges = 0
+
+    def add_vertix(self, vertix):
+        pass
+
+    def get_neighbors(self, vertix):
+        """
+        returns: [int]
+        """
+        pass
+
+
+class GraphAdjMatrix(Graph):
+
+    def adj_matrix(self):
+        """
+        returns: [[]]
+        """
