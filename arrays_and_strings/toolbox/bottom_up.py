753088d8601912040b31fdf90d2e6f47f8873f4f
John Shiver <johneshiver@gmail.com>
2016-08-03 23:03:18 -0400

    added another dynamic programming problem

--- arrays_and_strings/toolbox/bottom_up.py
+++ arrays_and_strings/toolbox/bottom_up.py
@@ -76,7 +76,6 @@ def change_possibilities_bottom_up(amount, denominations):
     # and progressively add each of our denominations
     ways_of_doing_n_cents[0] = 1
 
-    import ipdb;ipdb.set_trace()
     for coin in denominations:
         for higher_amount in range(coin, amount + 1):
             higher_amount_remainder = higher_amount - coin
@@ -134,3 +133,40 @@ for coin = 5:
 
 final answer: 3
 """
+
+
+def max_duffel_bag_value(cake_tuples, weight_capacity):
+    # we make a list to hold the maximum possible value at every
+    # duffel bag weight capacity from 0 to weight_capacity
+    # starting each index with value 0
+    max_values_at_capacities = [0] * (weight_capacity + 1)
+
+    for current_capacity in range(weight_capacity + 1):
+
+        # set a variable to hold the max monetary value so far for current_capacity
+        current_max_value = 0
+
+        for cake_weight, cake_value in cake_tuples:
+
+            # if a cake weighs 0 and has a positive value the value of our duffel bag is infinite!
+            if (cake_weight == 0 and cake_value != 0):
+                return float('inf')
+
+            # if the current cake weighs as much or less than the current weight capacity
+            # it's possible taking the cake would give get a better value
+            if (cake_weight <= current_capacity):
+                # so we check: should we use the cake or not?
+                # if we use the cake, the most kilograms we can include in addition to the cake
+                # we're adding is the current capacity minus the cake's weight. we find the max
+                # value at that integer capacity in our list max_values_at_capacities
+                max_value_using_cake = cake_value + max_values_at_capacities[current_capacity - cake_weight]
+
+                # now we see if it's worth taking the cake. how does the
+                # value with the cake compare to the current_max_value?
+                current_max_value = max(max_value_using_cake, current_max_value)
+
+        # add each capacity's max value to our list so we can use them
+        # when calculating all the remaining capacities
+        max_values_at_capacities[current_capacity] = current_max_value
+
+    return max_values_at_capacities[weight_capacity]
