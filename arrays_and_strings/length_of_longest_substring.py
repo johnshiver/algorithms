f05a0b0c7e87cb9c6310028f70319284d0343ce0
John Shiver <johneshiver@gmail.com>
2018-10-21 12:57:59 -0400

    added length of longest substring problem

--- arrays_and_strings/length_of_longest_substring.py
+++ arrays_and_strings/length_of_longest_substring.py
@@ -1,40 +1,69 @@
+"""
+Prompt:
+
+Given a string, find the length of the longest substring without repeating
+characters.
+"""
+"""
+start, end = 0, 0
+
+[a, b, c, a, b, c, d]
+          s
+    e
+
+[a b c b d]
+     s
+       e
+
+[p, w, w, k, e, w]
+       s
+       e
+
+o h v e h j d m l
+    s
+
+        e
+
+"""
 
 
 def length_of_longest_substring(s):
-    """Finds length of longest substring without repeating characters.
+    cache = [0] * 256
 
-    :param s:
-    :return:
-    """
-    i = j = 0
-    chars = set()
+    start = 0
     longest = 0
-    while j < len(s):
-        if s[j] not in chars:
-            chars.add(s[j])
-            j += 1
+    for i in range(len(s) + 1):
+        if i == len(s):
+            longest = max(longest, i - start)
+            break
+        c_position = ord(s[i])
+        if cache[c_position] == 0:
+            cache[c_position] = 1
         else:
-            longest = max(longest, len(chars))
-            # get rid of current char because it's a dupe
-            # re-added on next loop
-            chars.remove(s[j])
-            # move first pointer to dupe char
-            # and pop chars off set
-            while s[i] != s[j]:
-                chars.remove(s[i])
-                i += 1
-            i += 1
-    return max(longest, len(chars))
+            longest = max(longest, i - start)
+            while s[start] != s[i]:
+                cache[ord(s[start])] = 0
+                start += 1
+            # if they are equal, dont want to advance start otherwise you do
+            if start < i:
+                start += 1
+            else:
+                start = i
+                cache[c_position] = 1
+    return longest
 
 
-def longest_substring(self, s, k):
-    """
-    length of longest substring with at least k chars
-    """
-    for c in set(s):
-        if s.count(c) < k:
-            return max(self.longestSubstring(t, k) for t in s.split(c))
-    return len(s)
+test_cases = [
+    ("abcabcd", 4),
+    ("au", 2),
+    ("bbbbb", 1),
+    (" ", 1),
+    ("ohvhjdml", 6),
+]
 
 if __name__ == "__main__":
-    print(length_of_longest_substring("advd"))
+    for test_input, expected_output in test_cases:
+        actual_output = length_of_longest_substring(test_input)
+        if actual_output != expected_output:
+            print("Didnt get right output: {} {}, got {}".format(
+                test_input, expected_output, actual_output))
