77faf339e2fd6d27b712fc0130129320c174dfc5
John Shiver <johneshiver@gmail.com>
2018-10-22 23:47:07 -0400

    improved length of longest substring

--- arrays_and_strings/length_of_longest_substring.py
+++ arrays_and_strings/length_of_longest_substring.py
@@ -4,53 +4,24 @@ Prompt:
 Given a string, find the length of the longest substring without repeating
 characters.
 """
-"""
-start, end = 0, 0
-
-[a, b, c, a, b, c, d]
-          s
-    e
-
-[a b c b d]
-     s
-       e
-
-[p, w, w, k, e, w]
-       s
-       e
-
-o h v e h j d m l
-    s
-
-        e
-
-"""
 
 
 def length_of_longest_substring(s):
-    cache = [0] * 256
 
-    start = 0
-    longest = 0
-    for i in range(len(s) + 1):
-        if i == len(s):
-            longest = max(longest, i - start)
-            break
-        c_position = ord(s[i])
-        if cache[c_position] == 0:
-            cache[c_position] = 1
-        else:
-            longest = max(longest, i - start)
-            while s[start] != s[i]:
-                cache[ord(s[start])] = 0
-                start += 1
-            # if they are equal, dont want to advance start otherwise you do
-            if start < i:
-                start += 1
-            else:
-                start = i
-                cache[c_position] = 1
-    return longest
+	if len(s) < 2:
+		return len(s)
+
+	last_seen_cache = [-1]*256
+	start = longest = 0
+	for i, char in enumerate(s):
+		pos = ord(char)
+		last_seen = last_seen_cache[pos]
+		if last_seen >= start:
+			longest = max(longest, i-start)
+			start = last_seen + 1
+		last_seen_cache[pos] = i
+
+	return max(longest, (i-start)+1)
 
 
 test_cases = [
